7a1d3cf93627d930a5690e5ce7e13fedb98b908c
Ravindu Jayakodi <109682931+ravindujayakodi00@users.noreply.github.com>
2023-01-13 23:32:49 +0530

    User widget on home page

--- frontend/src/scenes/homePage/index.jsx
+++ frontend/src/scenes/homePage/index.jsx
@@ -1,10 +1,34 @@
-import { Box } from '@mui/material';
+import { Box, useMediaQuery } from '@mui/material';
+import { useSelector } from 'react-redux';
 import Navbar from "../navbar/index";
+import UserWidget from 'scenes/widgets/UserWidget';
 
 const HomePage = () => {
+    const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
+
     return (
         <Box>
             <Navbar />
+            <Box
+                display={isNonMobileScreen ? "flex" : "block"}
+                justifyContent="space-between"
+                width="100%"
+                padding="2rem 6%"
+                gap="0.5rem"
+            >
+                <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+                    <UserWidget userId={_id} picturePath={picturePath} />
+                </Box>
+                <Box 
+                    flexBasis={isNonMobileScreen ? "42%" : undefined}
+                    mt={isNonMobileScreen ? undefined : "2rem"}
+                >
+                    {/* widget */}
+                </Box>
+                {isNonMobileScreen && <Box flexBasis="26%"></Box>}
+            </Box>
+
         </Box>
     );
 }
